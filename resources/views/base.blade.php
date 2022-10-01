527214807ab540e592161444867b6e6a6d5a62e6
Adama@455 <1994adamandiaye@gmail.com>
2022-10-01 20:02:47 +0000

    Do not display footer about us

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,8 +13,10 @@
     @yield('contenu')
 
         <footer>
-            <p>&copy; Copyright {{date("Y")}} &middot; 
-            <a href="{{route('about')}}">About Us</a></p>
-        </footer>
+            <p>&copy; Copyright {{date("Y")}} 
+            @if(!Route::is ('about'))
+                &middot; <a href="{{route('about')}}">About Us</a></p>
+            @endif
+            </footer>
     </body>
 </html>
