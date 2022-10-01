ad733a111fc784c63b52df1c0c5e1cb567aeb5c3
Adama@455 <1994adamandiaye@gmail.com>
2022-10-01 13:12:23 +0000

    Move static page to page folder and remove link from about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -15,7 +15,7 @@
 
 
         <footer>
-            <p>&copy; Copyright {{date("Y")}} &middot; <a href="/about-us">About Us</a></p>
+            <p>&copy; Copyright {{date("Y")}} &middot; </p>
         </footer>
     </body>
 </html>
