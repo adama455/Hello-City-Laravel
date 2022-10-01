df9526dfa7eb9b7daf6bb4bce506d0952be5c2ee
Adama@455 <1994adamandiaye@gmail.com>
2022-10-01 13:29:18 +0000

    renove dupplication by using app leaout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About-us | hello city</title>
-        
+@section('contenu')
 
-    </head>
-    <body class="">
+    <p>Build with &hearts; by LES APPRENENTS.</p>
+    <p><a href="/"> Revenir à la page d'accueil</a></p>
 
-        <p>Build with &hearts; by LES APPRENENTS.</p>
-        <p><a href="/"> Revenir à la page d'accueil</a></p>
-
-
-        <footer>
-            <p>&copy; Copyright {{date("Y")}} &middot; </p>
-        </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Hello City</title>
-        
+@section('contenu')
+    <h1>Hello from Sénégal!</h1>
 
-    </head>
-    <body class="">
-        <h1>Hello from Sénégal!</h1>
+    <p>It's currently {{date("H:i A")}} .</p>
 
-        <p>It's currently {{date("H:i A")}} .</p>
+@endsection
 
-
-        <footer>
-            <p>&copy; Copyright {{date("Y")}} &middot; <a href="/about-us">About Us</a></p>
-        </footer>
-    </body>
-</html>
