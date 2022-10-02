b2a76c85cc193fc298c994be1ec63a78b9359dc4
Adama@455 <1994adamandiaye@gmail.com>
2022-10-02 01:47:51 +0000

    rectif

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/',function(){
-    return view('pages/home'); 
-})->name('home');
+Route::get('/', fn() => view('pages/home'))->name('home');
+Route::get('/about-us',fn() => view('pages/about'))->name('about');
 
-Route::get('/about-us',function(){
-    return view('pages/about');
-})->name('about');
+// Route::get('/about-us',function(){
+//     return view('pages/about');
+// })->name('about');
